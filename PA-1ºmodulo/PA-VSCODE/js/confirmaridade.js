9c2873f317969715e0a4f829c50eca533f913dbc
flavio <flaviodahsilva23@gmail.com>
2025-04-29 22:20:51 -0300

    bom dia
    oioi

---
+++ PA-1ºmodulo/PA-VSCODE/js/confirmaridade.js
@@ -0,0 +1,13 @@
+var idade= Number(prompt("Digite sua idade: "));
+
+
+if(idade==0){
+    alert("fdp ainda n nasceu porra ta emocionado esse como q pode")
+}
+else if(idade<=17){
+    alert("criança")
+}else if(idade<=64){
+    alert("jovem")
+}else{
+    alert("idoso")
+}
---
+++ PA-1ºmodulo/PA-VSCODE/js/script.js
@@ -0,0 +1,4 @@
+var media = (a+b+c)/3;
+
+
+console.log("oi")
